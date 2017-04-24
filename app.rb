0cd4883f16d146a46f729be7357dfd11e3310d40
Ben Metzger <benjpac@gmail.com>
2017-04-24 10:08:52 -0700

    now loop within the result erb

--- app.rb
+++ app.rb
@@ -11,10 +11,6 @@ end
 get('/anagrams_result') do
   @first_word = params.fetch("first_word")
   @second_word = params.fetch("second_word")
-  tests = @first_word.all_tests(@second_word)
-  @display = []
-  tests.each() do |test|
-    @display.push(test)
-  end
+  @display = @first_word.all_tests(@second_word)
   erb(:anagrams_result)
 end
